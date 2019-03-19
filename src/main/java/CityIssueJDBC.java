604a188b9906dd2140f99024ab37a4ef4d1bc91d
Andrii Kolomiiets <andrew.kolomiyets@gmail.com>
2019-03-19 09:57:06 +0200

    Initial commit

---
+++ src/main/java/CityIssueJDBC.java
@@ -0,0 +1,101 @@
+import java.sql.*;
+import org.apache.logging.log4j.*;
+
+public class CityIssueJDBC {
+    static CityIssueJDBC jdbcInstance;
+    private String url;
+    private String login;
+    private String password;
+    PreparedStatement preparedStatement;
+    private Connection connection;
+
+    private static final Logger logger = LogManager.getLogger(CityIssueJDBC.class);
+
+    //Private constructor and getInstance() for Singleton ensure;
+    private CityIssueJDBC() {
+    }
+
+    public static CityIssueJDBC getInstance() {
+        if (jdbcInstance == null) {
+            synchronized (CityIssueJDBC.class) {
+                jdbcInstance = new CityIssueJDBC();
+               logger.info("JDBC instance was created.");
+            }
+        }
+        return jdbcInstance;
+    }
+
+    /*        Adding mySQL via IntellijIdea:
+            File->Project Structure->Libraries
+            Then Click on the plus(+) sign and select From Meven....
+            After you'll get a search box there you should put
+            mysql:mysql-connector-java:5.1.40
+    */
+    public void connectToDB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            url = "jdbc:mysql://localhost/city-issue_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            login = "root";
+            password = "root784512";
+            connection = DriverManager.getConnection(url, login, password);
+            logger.trace("Connection successful!");
+        } catch (
+                Exception ex) {
+            logger.error("Connection failed...");
+            logger.error(ex);
+        }
+    }
+
+    public void connectToDB(String url, String login, String pass) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, login, pass);
+            logger.trace("Connection successful!");
+        } catch (
+                Exception ex) {
+            logger.error("Connection failed...");
+            logger.error(ex);
+        }
+    }
+
+    public void buildUserInfoIntoDB(String firstName, String lastName, String phone, String email) {
+        try {
+            connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            logger.error("AutoCommit can't be turned of...");
+            e.printStackTrace();
+        }
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO users(user_name, user_last_name, phone, email) VALUES (?, ?, ?, ?)");
+        preparedStatement.setString(1, firstName);
+        preparedStatement.setString(2, lastName);
+        preparedStatement.setString(3, phone);
+        preparedStatement.setString(4, email);
+        preparedStatement.addBatch();
+        preparedStatement.executeBatch();
+        preparedStatement.close();
+        logger.trace("Contact: " + firstName + " "+ lastName + " was created successfully.");
+        } catch (SQLException e) {
+            logger.error("Something goes wrong with the DB...");
+            e.printStackTrace();
+        }
+        try {
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            logger.error("AutoCommit can't be turned on...");
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            logger.error("Closing connection failed.");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/CityIssueJdbcTest.java
@@ -0,0 +1,33 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class CityIssueJdbcTest {
+    static String firstName;
+    static String lastName;
+    static String phone;
+    static String email;
+
+    public static void main(String[] args) {
+        CityIssueJDBC cityIssueJDBC = CityIssueJDBC.getInstance();
+        cityIssueJDBC.connectToDB();
+        getUserInput();
+        cityIssueJDBC.buildUserInfoIntoDB(firstName, lastName, phone, email);
+        cityIssueJDBC.closeConnection();
+    }
+
+    public static void getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first name: ");
+        firstName = scanner.nextLine();
+
+        System.out.println("Enter your last name: ");
+        lastName = scanner.nextLine();
+
+        System.out.println("Enter your phone: ");
+        phone = scanner.nextLine();
+
+        System.out.println("Enter your email: ");
+        email = scanner.nextLine();
+    }
+}
+
